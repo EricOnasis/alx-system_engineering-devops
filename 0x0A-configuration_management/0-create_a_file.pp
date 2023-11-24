effb31fada7a913f0478feb900f306c0ed73b1a2
Eric Onasis <ericonasis@gmail.com>
2023-11-24 13:31:47 +0300

    File creation in Puppet

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# create a file in with the following attributes
+
+file { 'school':
+  content => 'I love Puppet',
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+}
