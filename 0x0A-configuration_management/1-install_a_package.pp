40de56b2de596ede477706a7b0a35088c8b91115
Eric Onasis <ericonasis@gmail.com>
2023-11-25 18:20:56 +0300

    Fix

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,14 +3,11 @@
 # Install Flask using Puppet
 
 package { 'werkzeug':
-  ensure   => '1.0.1',  # Use a version that is compatible with Flask 2.1.0
+  ensure   => '1.0.1',
   provider => 'pip3',
 }
-<<<<<<< HEAD
-=======
 
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
->>>>>>> 480b7b0 (Fix)
