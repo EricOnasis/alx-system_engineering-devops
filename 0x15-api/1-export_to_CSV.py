d7f76725f4866034dc86723821026e5de2e971ee
Eric Onasis <ericonasis@gmail.com>
2024-01-05 02:16:25 +0300

    Fix Tak 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,17 +1,41 @@
 #!/usr/bin/python3
-"""Script to export data in the CSV format"""
+"""
+Script to export data in the CSV format.
+"""
+
 import csv
-import requests as r
+import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = "https://jsonplaceholder.typicode.com/"
-    usr = r.get(url + "users/{}".format(user_id)).json()
-    username = usr.get("username")
-    to_do = r.get(url + "todos", params={"userId": user_id}).json()
 
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        [writer.writerow([user_id, username, elm.get("completed"),
-                          elm.get("title")]) for elm in to_do]
+    def make_employee_request(resource, param=None):
+        """
+        Retrieve employee from API
+        """
+        url = 'https://jsonplaceholder.typicode.com/'
+        url += resource
+        if param:
+            url += ('?' + param[0] + '=' + param[1])
+
+        # request
+        res = requests.get(url)
+
+        # json response
+        res = res.json()
+        return res
+
+    user = make_employee_request('users', ('id', sys.argv[1]))[0]
+    tasks = make_employee_request('todos', ('userId', sys.argv[1]))
+
+    csv_filename = sys.argv[1] + '.csv'
+    with open(csv_filename, mode='w') as f:
+        writer = csv.writer(f,
+                            delimiter=',',
+                            quotechar='"',
+                            quoting=csv.QUOTE_ALL)
+        for task in tasks:
+            writer.writerow([user['id'],
+                            user['username'],
+                            task['completed'],
+                            task['title']])
