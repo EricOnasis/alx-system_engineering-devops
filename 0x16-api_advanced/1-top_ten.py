2ac20241a538aa60af6ab1df4b2f24f48e1183f8
Eric Onasis <ericonasis@gmail.com>
2024-01-17 05:54:07 +0300

    Fix

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""Module"""
+"""Module for task 1"""
 
 
 def top_ten(subreddit):
-    """Returns the top 10 hot posts
-    of a subreddit"""
+    """Queries the Reddit API and returns the top 10 hot posts
+    of the subreddit"""
     import requests
 
     sub_info = requests.get("https://www.reddit.com/r/{}/hot.json?limit=10"
